a82a46e7da7570a5e387a590591f9d4a86d16784
Alfonso <alfonxere@gmail.com>
2025-12-16 17:11:20 +0100

    16/12 v2

--- minas.py
+++ minas.py
@@ -20,30 +20,27 @@ def crearTablero(tamaño, numMinas):
 
 
 def minas_cercanas(tablero):
-    # Obtenemos las dimensiones usando el atributo shape
+
     filas = tablero.shape[0]
     columnas = tablero.shape[1]
     
     for fila in range(filas):
         for columna in range(columnas):
-            #Si la casilla actual es una bomba, no la numeramos
+            
             if tablero[fila, columna] == -1:
                 continue
             
             cont = 0
-            # Recorremos el vecindario de 3x3 (de -1 a +1)
+           
             for i in range(-1, 2):
                 for j in range(-1, 2):
                     nueva_fila = fila + i
                     nueva_columna = columna + j
-                    
-                    #Nos aseguramos que la nueva posición esté dentro del tablero
+                                        
                     if 0 <= nueva_fila < filas and 0 <= nueva_columna < columnas:
-                        #Si el vecino es una bomba, sumamos al contador
                         if tablero[nueva_fila, nueva_columna] == -1:
                             cont += 1
             
-            #Guardamos el total de minas alrededor en la casilla actual
             tablero[fila, columna] = cont
             
     return tablero
