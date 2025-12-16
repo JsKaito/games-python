0e0d9c05359165e236c3ceed5ad7ed169fcfafe9
LuciaIlerna <jimenezmoralesluci@alumnos.alborfp.com>
2025-12-16 16:38:18 +0100

    Creación y comienzo de colocación de barcos (lista de barcos e intento de mostrar el tablero)

---
+++ hundirlaflota.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Crear tablero
+tablero = np.zeros((10, 10), dtype=int)
+
+# Lista de barcos (nombre, tamaño)
+barcos = [
+    ("Portaaviones", 5),
+    ("Acorazado", 4),
+    ("Crucero 1", 3),
+    ("Crucero 2", 3),
+    ("Destructor", 2)
+]
+
+for barco, tamano in barcos:
+
+    colocado = False
+    while not colocado:
+
+        # Mostrar tablero
+        print("\n  1 2 3 4 5 6 7 8 9 10")
+        for i in range(10):
+            print(chr(ord('A') + i), end=" ")
+            for j in range(10):
+                if tablero[i][j] == 0:
+                    print("~", end=" ")
+                else:
+                    print("■", end=" ")
+            print()
+
+        print(f"\nColoca tu {barco} (tamaño {tamano})")
+print(tablero)
