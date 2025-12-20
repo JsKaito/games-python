d2ca427c3dd4ab63621bc976ecadeb1146d2738c
Kaito <fernandoruizmurciano@gmail.com>
2025-12-20 14:03:49 +0100

    ✨ Cambio: Se añadió la importación de la librería random para generar movimientos aleatorios.
    🔧 Arreglado: Se corrigió la función actualizarMovimientos para manejar correctamente los movimientos del usuario y de la IA.
    
    📝 Cambio: Se reestructuró el flujo de la función pensarMovimiento para asegurar que los movimientos no se repitan.

--- hundir_la_flota.py
+++ hundir_la_flota.py
@@ -10,6 +10,7 @@
 #
 
 import numpy as np
+import random as rd
 
 # Función de creación del tablero
 def crearTablero(tamaño):
@@ -17,36 +18,46 @@ def crearTablero(tamaño):
     tablero = np.zeros((tamaño, tamaño), dtype=int)
     return tablero
     
+
+def actualizarMovimientos(movimiento, esUsuario): # FALTA DECLARAR MOVIMIENTO
+    
+    if esUsuario:
+        movimientosHechosUsuario.append(movimiento)
+    else:
+        movimientosHechosIA.append(movimiento) # VIENE DE LA FUNCIÓN DE ATAQUE
+
+
+def pensarMovimiento(tableroUsuario):
+    while True:
+        
+        movimiento = (rd.choice("ABCDEFGHIJ"), rd.randint(1, 10))
+        if movimiento not in movimientosHechosIA:
+            break
+        
+        if movimiento == 0:
+            print() # Fallo
+        
+        elif movimiento == 1:
+            print() # Tocado
+        
+        elif movimiento == 2:
+            print() # Hundido
+            
+            
+        
 tableroIA = crearTablero(10)
 tableroUsuario = crearTablero(10)
-movimientosHechosIA = []
+movimientosHechosIA, movimientosHechosUsuario = [], []
 
 # 0 = FALLO
 # 1 = TOCADO
 # 2 = HUNDIDO
 
-
-
 movimiento = (1, "A")
-
-def actualizarMovimientos(tableroUsuario):
-    
-    movimientosHechosIA.append(movimiento) # VIENE DE LA FUNCIÓN DE ATAQUE
-
-
-def pensarMovimiento(tableroUsuario):
-    if (movimiento not in movimientosHechosIA) and (movimiento == 0):
-        
-            
-        
-        
         
 
-print(crearTablero(10))
+# TODO (Lucia): Función para colocar barcos del usuario -- Lucia
+# TODO (No asignado): Función de ataque del usuario / IA
+# TODO (Fer): Función para colocar barcos de la IA
+# TODO (Fer):  Función de pensar el ataque de la IA
 
-# TODO: 
-
-# - Función para colocar barcos del usuario  -- Lucia
-# - Función de ataque del usuario / IA
-# - Función para colocar barcos de la IA
-# - Función de pensar el ataque de la IA
