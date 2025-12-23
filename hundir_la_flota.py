b529ae667bc5233aaea3e1c70c4c7895b3332888
Kaito <fernandoruizmurciano@gmail.com>
2025-12-23 04:55:30 +0100

    ✨ Añadir lógica de movimientos de IA
    FEATURE: Crear arrays de coordenadas completas para IA y usuario
    CHANGE: Actualizar función de pensamiento de IA para manejar movimientos (WIP)

--- hundir_la_flota.py
+++ hundir_la_flota.py
@@ -12,12 +12,24 @@
 import numpy as np
 import random as rd
 
+tamaño = 10
+letras = "ABCDEFGHIJ"
+opcionesIA, opcionesUser = [], []
+
+
+# Creación de array de coordenadas completas (Para reducir las opciones de coordenadas cuando se vayan usando)
+for i in range(1, tamaño + 1):
+    for char in letras:
+        opcionesIA.append((char, i))
+        opcionesUser.append((char, i))
+        
 # Función de creación del tablero
 def crearTablero(tamaño):
 
     tablero = np.zeros((tamaño, tamaño), dtype=int)
     return tablero
     
+
 
 def actualizarMovimientos(movimiento, esUsuario): # FALTA DECLARAR MOVIMIENTO
     
@@ -27,24 +39,28 @@ def actualizarMovimientos(movimiento, esUsuario): # FALTA DECLARAR MOVIMIENTO
         movimientosHechosIA.append(movimiento) # VIENE DE LA FUNCIÓN DE ATAQUE
 
 
+# Función de pensamiento de movimiento de IA
 def pensarMovimiento(tableroUsuario):
+    
     while True:
         
-        movimiento = (rd.choice("ABCDEFGHIJ"), rd.randint(1, 10))
-        if movimiento not in movimientosHechosIA:
-            break
+        if ultimoMovimiento == 0:
+            
+            # Movimiento completo al azar para ponerlo si el ultimo movimiento fue un fallo
+            nuevoMovimiento = (rd.choice("ABCDEFGHIJ"), rd.randint(1, 10))
+            if nuevoMovimiento not in movimientosHechosIA:
+                break
         
-        if movimiento == 0:
-            print() # Fallo
-        
-        elif movimiento == 1:
+        elif ultimoMovimiento == 1:
             print() # Tocado
         
-        elif movimiento == 2:
+        elif ultimoMovimiento == 2:
             print() # Hundido
             
             
-        
+print(coordsIA)
+            
+'''
 tableroIA = crearTablero(10)
 tableroUsuario = crearTablero(10)
 movimientosHechosIA, movimientosHechosUsuario = [], []
@@ -54,7 +70,16 @@ movimientosHechosIA, movimientosHechosUsuario = [], []
 # 2 = HUNDIDO
 
 movimiento = (1, "A")
-        
+        '''
+
+# TODO: Patrón último movimiento para programación de la IA
+# TODO: Arrays de todos los movimientos y movimientos ya hechos para optimizar el pensamiento de la IA y que no funcione con brute-force.
+# ! Cuando cree los arrays funcionales de las coordenadas completas y use los movimientos para reducirlo, cambiar los nombres de las variables y métodos deprecados.
+
+
+
+
+
 
 # TODO (Lucia): Función para colocar barcos del usuario -- Lucia
 # TODO (No asignado): Función de ataque del usuario / IA
