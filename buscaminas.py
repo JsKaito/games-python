9c891f05430ec3f81ca33805e893439b50bd6aaf
rocioramirezsanroman2006-a11y <rocioramirezsanroman2006@alumnos.alborfp.com>
2026-01-27 17:48:44 +0100

    MENU

--- buscaminas.py
+++ buscaminas.py
@@ -1,12 +1,35 @@
-# 1. Definimos el tamaño
-filas = 9
-columnas = 9
+import string
 
-# 2. Creamos el tablero (una lista que tiene listas adentro)
-tablero = []
-for i in range(filas):
-    fila = ["."] * columnas  
-    tablero.append(fila)  
-    
-print(tablero)
+class Interfaz:
+    def __init__(self):
+        # Inicializar interfaz
+        self.juego = None
 
+    def imprimir_tablero(self, tablero):
+        # Imprimir tablero en pantalla
+        tamaño = tablero.shape[0]
+        letras = string.ascii_uppercase[:tamaño]
+        print("\n      " + "  ".join(letras))
+        for i, fila in enumerate(tablero):
+            print(f" {i+1:2d} | " + "  ".join(str(x) for x in fila))
+        print()
+
+    def menu_principal(self):
+        # Menú principal
+        while True:
+            print("\n" + "="*35)
+            print("     BUSCAMINAS")
+            print("="*35)
+            print("1. Jugar")
+            print("2. Salir")
+            print("="*35)
+            opcion = input("Opcion: ").strip()
+            if opcion == "1":
+                self.jugar()
+            elif opcion == "2":
+                print("\nAdios\n")
+                break
+            else:
+                print("Opcion invalida")
+
+  
