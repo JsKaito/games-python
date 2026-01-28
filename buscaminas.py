24476bbffe13122c09c8113f86dac037ec8a2d88
rocioramirezsanroman2006-a11y <rocioramirezsanroman2006@alumnos.alborfp.com>
2026-01-28 15:32:36 +0100

    menu

--- buscaminas.py
+++ buscaminas.py
@@ -33,18 +33,15 @@ class Interfaz:
             else:
                 print("Opcion invalida")
 
-  
-=======
 # Definimos el tamaño 
 filas = 9
 columnas = 9
 
-# Creamos el tablero lleno de puntos '.' (que son las casillas tapadas)
+# Creamos el tablero lleno de puntos '
 tablero = []
 for i in range(filas):
     fila = ["."] * columnas
     tablero.append(fila)
 
-# Se puede ver el principio
 print(tablero)
 
